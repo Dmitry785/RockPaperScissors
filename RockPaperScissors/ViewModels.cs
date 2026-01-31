dd077cb7fae44f3e863678e331c430cea6310034
Vladimir Sviridov <ssViridd@gmail.com>
2026-01-31 17:02:37 +0700

    add viewmodels

---
+++ RockPaperScissors/ViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+    public class RockPaperScissorsViewModel : BaseViewModel
+    {
+
+    }
+}
