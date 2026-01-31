fd1d616ac573486b0a8fbfcf5678c0ef2553d45f
Vladimir Sviridov <ssViridd@gmail.com>
2026-01-31 16:54:03 +0700

    add models

--- RockPaperScissors/MainWindow.xaml.cs
+++ RockPaperScissors/MainWindow.xaml.cs
@@ -11,9 +11,6 @@ using System.Windows.Shapes;
 
 namespace RockPaperScissors
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
---
+++ RockPaperScissors/Models.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public abstract class Item
+    {
+        public abstract BattleResult Battle(Item item);
+    }
+    public class Rock : Item
+    {
+        public override BattleResult Battle(Item item)
+        {
+            if(item is Paper)
+                return BattleResult.Fail;
+            if(item is Scissors)
+                return BattleResult.Win;
+            return BattleResult.Draw;
+        }
+    }
+    public class Paper : Item
+    {
+        public override BattleResult Battle(Item item)
+        {
+            if (item is Scissors)
+                return BattleResult.Fail;
+            if (item is Rock)
+                return BattleResult.Win;
+            return BattleResult.Draw;
+        }
+    }
+    public class Scissors : Item
+    {
+        public override BattleResult Battle(Item item)
+        {
+            if (item is Rock)
+                return BattleResult.Fail;
+            if (item is Paper)
+                return BattleResult.Win;
+            return BattleResult.Draw;
+        }
+    }
+    public enum BattleResult
+    {
+        Win,
+        Draw,
+        Fail
+    }
+}
